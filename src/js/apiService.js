5a12599f3f9fe68d040092364576da4496c4c0c7
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-05-31 15:56:59 +0300

    вроде всьооо

---
+++ src/js/apiService.js
@@ -0,0 +1,33 @@
+  
+export default class ImageApiService {
+    constructor() {
+      this.searchQuery = '';
+      this.page = 1;
+    }
+  
+    async fetchImages() {
+      const API_KEY = '21770810-71d636d270901957bd723d7c5';
+      const BASE_URL = 'https://pixabay.com/api';
+  
+      const response = await fetch(
+        `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+      );
+  
+      const newImages = await response.json();
+  
+      this.page += 1;
+      return newImages.hits;
+    }
+  
+    resetPage() {
+      this.page = 1;
+    }
+  
+    get query() {
+      return this.searchQuery;
+    }
+  
+    set query(newQuery) {
+      this.searchQuery = newQuery;
+    }
+  }
